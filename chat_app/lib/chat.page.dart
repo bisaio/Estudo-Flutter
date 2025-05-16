114346ca5def65d56acf86cb3b1a330051b9523e
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2025-05-16 17:36:58 -0300

    finalizando o chat

--- chat_app/lib/chat.page.dart
+++ chat_app/lib/chat.page.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class ChatPage extends StatelessWidget {
+  ChatPage() {
+    timeago.setLocaleMessages('pt_br', timeago.PtBrMessages());
+  }
+
   final _auth = FirebaseAuth.instance;
   final _db = FirebaseFirestore.instance;
 
@@ -14,9 +19,12 @@ class ChatPage extends StatelessWidget {
       'user': _auth.currentUser!.displayName,
       'uid': _auth.currentUser!.uid,
       'timestamp': FieldValue.serverTimestamp(),
+      'status': 'created'
     };
 
     await _db.collection('messages').add(data);
+
+    txtMessage.text = '';
   }
 
   @override
@@ -34,15 +42,49 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Flexible(
-            child: ListView(
-              children: [
-                ListTile(
-                  leading: CircleAvatar(),
-                  title: Text("Fulano"),
-                  subtitle: Text("Mensagem..."),
-                  trailing: Text("10/4"),
-                ),
-              ],
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream:
+                  _db
+                      .collection('messages')
+                      .where('status', isNotEqualTo: 'deleted')
+                      .orderBy('timestamp', descending: true)
+                      .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Text("Carregando...");
+
+                return ListView(
+                  reverse: true,
+                  children:
+                      snapshot.data!.docs
+                          .map(
+                            (doc) => Dismissible(
+                              // confirmDismiss: (_) async {
+                              //   if (_auth.currentUser!.uid == doc['uid']) return true;
+                              //   return false;
+                              // },
+                              background: Container(
+                                color: Colors.red,
+                                child: Icon(Icons.delete_outline_outlined, color: Colors.white, ),
+                              ),
+                              key: Key(doc.id),
+                              // onDismissed: (_) => doc.reference.delete(),
+                              onDismissed: (_) => doc.reference.update({'status':'deleted'}),
+                              child: ListTile(
+                                leading: CircleAvatar(),
+                                title: Text(doc['user']),
+                                subtitle: Text(doc['message']),
+                                trailing: Text(
+                                  timeago.format(
+                                    doc['timestamp'].toDate(),
+                                    locale: 'pt_br',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                          .toList(),
+                );
+              },
             ),
           ),
           Container(
